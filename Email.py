6ac82ca0a241783c015d54fb3f3db4e89e1128ab
Ebikhalu Grace <2ebikhalu@gmail.com>
2025-01-22 15:47:46 +0100

    Add files via upload
    This is the code

---
+++ Email.py
@@ -0,0 +1,85 @@
+"""
+birth = input('date of birth in order yyyymmdd')
+
+year = birth[0:4]
+Month = birth[4:6]
+date = birth[6:]
+#age = 2024 - year 
+
+age =  2024 - int(year)
+print(f"you are {age} ")
+
+"""
+
+"""
+user_name = input('enter your username')
+
+user_name.isalnum(user_name )
+
+if user_name not :
+    print('your username is', [username])
+else:
+    print('input a valid username')
+
+"""
+
+sug = 'ABCDEFGHIabcdefghi1234567'
+
+sug_paswd = input('suggest a password (yes or no)')
+
+if sug_paswd == no:
+    print('password must be eight character password that contains alpha and num ')
+    password = input('enter your password')
+   
+nick_name = input('enter your username ')
+if nick_name == user_name:
+    pw = input('enter your password')
+    if pw == password:
+        print('welcome', (nick_name))
+    else:
+        print('wrong password')   
+elif pw ≠ password :
+    print('try again')
+    pw = input('enter your password')
+    if pw == password:
+        print('welcome', (nick_name))
+    else:
+        print('wrong password')   
+else:
+    print('forgot your password')
+    change = input('input your phone number ')
+    if change == phone_number:
+        print(nick_name)
+        new_password = input('enter new password')
+        replace(password) with new password
+    else:
+        print('wrong number')      
+else:
+    a = print(random(sug))
+    password2= a
+nick_name = input('enter your username ')
+if nick_name == user_name:
+    pw = input('enter your password')
+    if pw == password2:
+        print('welcome', (nick_name))
+    else:
+        print('wrong password')   
+elif pw ≠ password2 :
+    print('try again')
+    pw = input('enter your password')
+    if pw == password2:
+        print('welcome', (nick_name))
+    else:
+        print('wrong password')   
+else:
+    print('forgot your password')
+    change = input('input your phone number ')
+    if change == phone_number:
+        print(nick_name)
+        new_password = input('enter new password')
+        replace(password) with new password
+    else:
+        print('wrong number')      
+        
+         
+    
